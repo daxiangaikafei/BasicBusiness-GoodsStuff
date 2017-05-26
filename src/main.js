93f0aaf7a8f23f8c527d0982e567613810224ecd
FIRST_NAME LAST_NAME <mail@xuechen.wang>
2017-05-26 17:09:47 +0800

    init project

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import routes from './router/router'
+import store from './vuex';
+Vue.use(VueRouter)
+const router = new VueRouter({
+	// routes
+	mode: 'history',
+  	routes: routes
+})
+
+new Vue({
+	router,
+	store
+}).$mount('#app')
