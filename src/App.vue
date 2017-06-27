b24919cfc229b79161cbadd5a37b152a7e656f7d
FIRST_NAME LAST_NAME <mail@xuechen.wang>
2017-06-27 17:02:54 +0800

    update files

--- src/App.vue
+++ src/App.vue
@@ -36,7 +36,7 @@ import { mapState ,mapMutations} from 'vuex';
         }).
         then(res =>{
           if(res.data.code == 0){
-            var appId = res.data.result.appId;
+            var appId = res.data.result.appid;
             // _vue.appId = res.data.result.appid;
             _vue.setAppId({appId});
           }
@@ -47,6 +47,7 @@ import { mapState ,mapMutations} from 'vuex';
             }).
             then(res => {
                 if(res.data.code != 0){
+                  console.log(_vue.appId);
                   //跳转至微信授权页面：https://open.weixin.qq.com/connect/oauth2/authorize?appid=APPID&redirect_uri=REDIRECT_URI&response_type=code&scope=SCOPE&state=STATE#wechat_redirect
                   //参数解释如下：state为重定向后需要添加的参数，redirect_url为重定向地址，我们这边统一为/login
                   // window.location.href = '/login?pageType=stuff';
